0bf780247ff3e48d2bb3f8634c1de5f780dad779
Jirehlov <34600796+Jirehlov@users.noreply.github.com>
2024-09-02 11:09:56 +0900

    Update 66.py

--- utils/66.py
+++ utils/66.py
@@ -12,10 +12,10 @@ filtered_df = df1_common[(df1_common['类型'] == 2)]
 with open('watching.txt', 'w', encoding='utf-8') as f:
     f.write(f'在看人数变化：\n')
     for index, row in filtered_df[filtered_df['diff'] > 5].sort_values('diff', ascending=False).iterrows():
-        f.write(f"{index}, {row['类型']}, {row['中文标题']}, {row[column_to_compare]} - {df2_common.loc[index, column_to_compare]} = {int(row['diff']):+d}\n")
+        f.write(f"{index}, {row['中文标题']}, {row[column_to_compare]} - {df2_common.loc[index, column_to_compare]} = {int(row['diff']):+d}\n")
     f.write('...\n')
     for index, row in filtered_df[filtered_df['diff'] < -5].sort_values('diff', ascending=False).iterrows():
-        f.write(f"{index}, {row['类型']}, {row['中文标题']}, {row[column_to_compare]} - {df2_common.loc[index, column_to_compare]} = {int(row['diff']):+d}\n")
+        f.write(f"{index}, {row['中文标题']}, {row[column_to_compare]} - {df2_common.loc[index, column_to_compare]} = {int(row['diff']):+d}\n")
     f.write('\n\n\n')
     total_sum_diff = filtered_df[column_to_compare].sum() - df2_common.loc[filtered_df.index, column_to_compare].sum()
     f.write(f"\n在看总变化：{filtered_df[column_to_compare].sum()} - {df2_common.loc[filtered_df.index, column_to_compare].sum()} = {total_sum_diff:+d}\n")
