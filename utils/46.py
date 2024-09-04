5a4696e84748cceea76bc76bdf76aebd06bd5e16
Jirehlov <34600796+Jirehlov@users.noreply.github.com>
2024-09-04 12:40:22 +0900

    Update 46.py

--- utils/46.py
+++ utils/46.py
@@ -1,6 +1,7 @@
 import json
 import csv
 type_counts = {t: {'json': 0, 'csv': 0} for t in ['1', '2', '3', '4', '6']}
+types = ['书籍', '动画', '音乐', '游戏', '', '三次元']
 with open('subject.jsonlines', 'r', encoding='utf-8') as file:
     for line in file:
         data = json.loads(line)
@@ -11,15 +12,15 @@ with open('sorted1.csv', 'r', encoding='utf-8-sig') as file:
     next(file)
     for row in csv.reader(file):
         type_str = row[1].strip()
-        if type_str in type_counts:
+        rank = float(row[-1].strip())
+        if int(rank) > 0 and type_str in type_counts:
             type_counts[type_str]['csv'] += 1
 total_json = total_csv = 0
-print("Counts and ratios by type:")
+print("VIB条目命中比例:")
 for t, counts in type_counts.items():
-    count_json, count_csv = counts['json'], counts['csv']
-    total_json += count_json
-    total_csv += count_csv
-    ratio = 100.0 * count_csv / count_json if count_json else 0
-    print(f"Type {t}: JSON count: {count_json}, CSV count: {count_csv}, Ratio: {ratio:.3f}%")
+    total_json += counts['json']
+    total_csv += counts['csv']
+    ratio = 100.0 * counts['csv'] / counts['json'] if counts['json'] else 0
+    print(f"{types[int(t)-1]}: 表面评分条目数: {counts['json']}, VIB条目数: {counts['csv']}, 占比: {ratio:.3f}%")
 overall_ratio = 100.0 * total_csv / total_json if total_json else 0
-print(f"\nOverall counts and ratio:\nTotal JSON count: {total_json}\nTotal CSV count: {total_csv}\nOverall ratio: {overall_ratio:.3f}%")
+print(f"总计：表面评分条目数 {total_json}, VIB条目数: {total_csv}, 总占比: {overall_ratio:.3f}%")
