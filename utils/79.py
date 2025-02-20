024af5be46d0295351d8241c6a9bce8131ce6700
Jirehlov <contact@jirehlov.com>
2025-02-20 11:27:29 +0900

    Update 79.py

--- utils/79.py
+++ utils/79.py
@@ -1,6 +1,6 @@
 import sys, json
 def compare_ids(json_file):
-    input_ids = {item['id'] for item in json.load(open(json_file, 'r', encoding='utf-8')) if 'id' in item}
+    input_ids = {item['subject_id'] for item in json.load(open(json_file, 'r', encoding='utf-8')) if 'subject_id' in item}
     subject_ids = {json.loads(line.strip())['id'] for line in open('subject.jsonlines', 'r', encoding='utf-8') if 'id' in json.loads(line.strip())}
     for diff_id in input_ids - subject_ids:
         print(f"https://bgm.tv/subject/{diff_id}")
